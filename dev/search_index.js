5e1d5f067c509a777e5c966f6aa5c96e4c34f611
Documenter.jl <documenter@juliadocs.github.io>
2026-08-25 07:57:42 +0000

    build based on 5dc0aab

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"category":"section","location":"","page":"Home","text":"@@meta CurrentModule = MatterMost @@","title":"Home"},{"category":"section","location":"#MatterMost","page":"Home","text":"MatterMost.jl provides the generated REST v4 API plus a stable client facade for application and ChatThemAll connector code.\n\n@@index @@\n\n@@autodocs Modules = [MatterMost] @@","title":"MatterMost"}]
+}
